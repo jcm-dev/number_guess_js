88b2439b8c4c4b0cde107d5fb37b60650c88a778
Joshua <meyer.josh8@gmail.com>
2021-02-26 09:42:28 -0600

    Update comments

--- app.js
+++ app.js
@@ -17,32 +17,34 @@ guessBtn.addEventListener('click', function(){
   // take user entry and convert to int and
   // assign to local variable
   let guess = parseInt(guessInput.value);
-
+  // test if input contains a valid number, and
+  // if so test if it is out of range
+  // if any of these is true then display the error message
   if(isNaN(guess) || guess < min || guess > max){
     setMessage(`Please enter a number betweeen ${min} and ${max}`, 'red');
   }
-
   // test if guess is equal to winning number
+  // and if true then display the game over winning message
+  // with the winning number passed in
   if(guess === winningNum){
-    // display game over
     gameOver(true, `${winningNum} is correct, YOU WIN!`);
   }else{
-    // wrong number
+    // if guess is the wrong number then
+    // decrement guessesLeft by 1
     guessesLeft -= 1;
-
+    // if guesses left is equal to 0
+    // then the game is over and display the 
+    // game over message passing false
     if (guessesLeft === 0){
-      // game over lose
-  
       gameOver(false, `Game Over, you lost. The correct number was ${winningNum}`);
-
     } else {
-      // game continues answer wrong
-          // change border color
+    // if guess left then set the input box
+    // border color to red and allow for more guesses
     guessInput.style.borderColor = 'red';
-
-    // clear input
+    // clear the input for the user to make another guess
     guessInput.value = '';
-    // tell user wrong number
+    // inform the user they entered the wrong number
+    // and how many guesses they have left
       setMessage(`${guess} is not correct, ${guessesLeft} guesses left`, 'red');
     }
   };
