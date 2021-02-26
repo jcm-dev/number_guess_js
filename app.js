968090fc26a0a2856ec177b4ace115663da4c5b9
Joshua <meyer.josh8@gmail.com>
2021-02-26 09:37:39 -0600

    Creeate guessBtn event listener

--- app.js
+++ app.js
@@ -12,3 +12,38 @@ const game = document.querySelector('#game'),
       guessInput = document.querySelector('#guess-input'),
       message = document.querySelector('.message');
 
+// GUESS LISTENER
+guessBtn.addEventListener('click', function(){
+  // take user entry and convert to int and
+  // assign to local variable
+  let guess = parseInt(guessInput.value);
+
+  if(isNaN(guess) || guess < min || guess > max){
+    setMessage(`Please enter a number betweeen ${min} and ${max}`, 'red');
+  }
+
+  // test if guess is equal to winning number
+  if(guess === winningNum){
+    // display game over
+    gameOver(true, `${winningNum} is correct, YOU WIN!`);
+  }else{
+    // wrong number
+    guessesLeft -= 1;
+
+    if (guessesLeft === 0){
+      // game over lose
+  
+      gameOver(false, `Game Over, you lost. The correct number was ${winningNum}`);
+
+    } else {
+      // game continues answer wrong
+          // change border color
+    guessInput.style.borderColor = 'red';
+
+    // clear input
+    guessInput.value = '';
+    // tell user wrong number
+      setMessage(`${guess} is not correct, ${guessesLeft} guesses left`, 'red');
+    }
+  };
+});
